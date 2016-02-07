472545c64d5ed5e09902947bbb536dec81614af2
James Ross <git@james-ross.co.uk>
2016-02-07 21:39:25 +0000

    Bug fix for outgoing tracing on new connections

--- init.js
+++ init.js
@@ -73,7 +73,7 @@ var DisplayDummyUser = {
 };
 
 function observeAfterConnect() {
-    this.connection[plugin.id + "-network"] = this;
+    this.connection[plugin.id + "-network"] = this.parent;
 }
 
 function observeAfterSendData(str) {
